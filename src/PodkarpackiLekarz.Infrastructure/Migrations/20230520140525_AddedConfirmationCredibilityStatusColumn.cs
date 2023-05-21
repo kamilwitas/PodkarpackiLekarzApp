0eb753bfb5413693c9016c9c820b4f2608d51f5f
Kamil Witas <kamilwitas1@gmail.com>
2023-05-21 12:39:56 +0200

    Added new migrations

---
+++ src/PodkarpackiLekarz.Infrastructure/Migrations/20230520140525_AddedConfirmationCredibilityStatusColumn.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PodkarpackiLekarz.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedConfirmationCredibilityStatusColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DoctorProfiles_DoctorTypes_DoctorTypeId",
+                schema: "PLA",
+                table: "DoctorProfiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_IdentityUsers_Email",
+                schema: "PLA",
+                table: "IdentityUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CredibilityConfirmed",
+                schema: "PLA",
+                table: "Doctors");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RefreshToken",
+                schema: "PLA",
+                table: "UserSessions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                schema: "PLA",
+                table: "IdentityUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                schema: "PLA",
+                table: "IdentityUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                schema: "PLA",
+                table: "IdentityUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                schema: "PLA",
+                table: "IdentityUsers",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Speciality",
+                schema: "PLA",
+                table: "DoctorTypes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CredibilityConfirmationStatus",
+                schema: "PLA",
+                table: "Doctors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DoctorTypeId",
+                schema: "PLA",
+                table: "DoctorProfiles",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "PLA",
+                table: "DoctorProfiles",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdentityUsers_Email",
+                schema: "PLA",
+                table: "IdentityUsers",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DoctorProfiles_DoctorTypes_DoctorTypeId",
+                schema: "PLA",
+                table: "DoctorProfiles",
+                column: "DoctorTypeId",
+                principalSchema: "PLA",
+                principalTable: "DoctorTypes",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DoctorProfiles_DoctorTypes_DoctorTypeId",
+                schema: "PLA",
+                table: "DoctorProfiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_IdentityUsers_Email",
+                schema: "PLA",
+                table: "IdentityUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CredibilityConfirmationStatus",
+                schema: "PLA",
+                table: "Doctors");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RefreshToken",
+                schema: "PLA",
+                table: "UserSessions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                schema: "PLA",
+                table: "IdentityUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                schema: "PLA",
+                table: "IdentityUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                schema: "PLA",
+                table: "IdentityUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                schema: "PLA",
+                table: "IdentityUsers",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Speciality",
+                schema: "PLA",
+                table: "DoctorTypes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CredibilityConfirmed",
+                schema: "PLA",
+                table: "Doctors",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DoctorTypeId",
+                schema: "PLA",
+                table: "DoctorProfiles",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "PLA",
+                table: "DoctorProfiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdentityUsers_Email",
+                schema: "PLA",
+                table: "IdentityUsers",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DoctorProfiles_DoctorTypes_DoctorTypeId",
+                schema: "PLA",
+                table: "DoctorProfiles",
+                column: "DoctorTypeId",
+                principalSchema: "PLA",
+                principalTable: "DoctorTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ src/PodkarpackiLekarz.Infrastructure/Migrations/20230520141557_AddedMedicalLicenseColumn.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PodkarpackiLekarz.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedMedicalLicenseColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MedicalLicenseNumber",
+                schema: "PLA",
+                table: "DoctorProfiles",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MedicalLicenseNumber",
+                schema: "PLA",
+                table: "DoctorProfiles");
+        }
+    }
+}
